a2f3e1c753f5f1ed6026bd10c304ac244552ef7f
Naga <jp38852885@gmail.com>
2022-06-13 12:10:49 +0530

    test

--- src/main/scala/scalapb/demo/Demo.scala
+++ src/main/scala/scalapb/demo/Demo.scala
@@ -2,7 +2,7 @@ package scalapb.demo
 
 import scalapb.protos.demo.Person
 
-object Demo {
+object emo {
   def main(args: Array[String]): Unit = {
     val person = Person(name="John", age=38)
     println(person.toByteArray.toVector)
